db4d396f7ea371f495df7c70a073330329617ab8
Jesper Vandborg <vandborg@LINUXMINT-ThinkPad-X121e.(none)>
2013-03-13 12:51:07 +0100

    first commit

---
+++ gen/ucn/datamatiker/vandborg/weatherapp/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package ucn.datamatiker.vandborg.weatherapp;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
