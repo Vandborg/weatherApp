16ea8c0f32ce19852b3aaf0776437a1bb90447ea
Jesper Vandborg <vandborg@LINUXMINT-ThinkPad-X121e.(none)>
2013-03-29 23:04:45 +0100

    Adding DetailsFragment.java

---
+++ src/ucn/datamatiker/vandborg/weatherapp/DetailsFragment.java
@@ -0,0 +1,13 @@
+package ucn.datamatiker.vandborg.weatherapp;
+
+import android.support.v4.app.Fragment;
+
+public class DetailsFragment extends Fragment {
+	
+	public void refreshWeather(){
+		//Getting the xml
+		
+		
+	}
+	
+}
